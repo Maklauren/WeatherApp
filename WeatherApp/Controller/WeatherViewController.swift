7e558737eba7e7518cdcb94a7d1d0117a30d65d9
Pavel Chernook <Maklauren97@gmail.com>
2022-06-30 19:12:11 +0300

    Implemented a weather screen

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Павел Черноок on 30.06.22.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
